9bdff7c8960e31776d1a9f153f3b31d71a2c5810
rapidlizard <kiko.cbg@gmail.com>
2021-01-18 19:36:53 +0100

    add admin test, disable register route, add type atribute to user, delete example test files

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,18 +8,8 @@ use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
@@ -29,5 +19,15 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function admin()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email' => 'admin@cancruz.com',
+                'type' => 'admin'
+            ];
+        });
     }
 }
